f5737a20ec5b0ce6c11622e9dfd87caa94b12b61
Tyler Rosen <tylermrosen@gmail.com>
2017-03-07 22:13:19 -0500

    Created array of quotes/lyrics, started writing mysql database

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE hangman_db;
+
+USE hangman_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL,
+    username varchar(230) NOT NULL,
+    email varchar(230) NOT NULL,
+    password_hash varchar(230) NOT NULL,
+)
+
