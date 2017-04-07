2c2a32378ef0fa5c8e8d0c9012f5df05d155eccf
Tyler Rosen <tylermrosen@gmail.com>
2017-04-07 12:16:25 -0400

    Current version

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -1,53 +1,6 @@
 //Global Variables
 // --------------------------------------------------------------------
 
-var lyrics = [
-    {
-        song:"Protect Ya Neck",
-        artist: "Wu Tang Clan",
-        lyric:"I smoke on the mic like Smokin' Joen Frazier, the hell-raiser, raising Hell with the flavor"
-    },
-
-    {
-        song:"Bad Romance",
-        artist: "Lady Gaga",
-        lyric:"Rah, rah, ah, ah, ah, roma, roma, ma. Gaga, ooh, la, la... want your bad romance!"
-    },
-
-      {
-        song:"Lose Yourself",
-        artist: "Eminem",
-        lyric:"There's vomit on his sweater already, mom's spaghetti!"
-    },
-
-    {
-        song: "Square Hammer",
-        artist: "Ghost",
-        lyric: "Are you on the square? Are you on the level? Are you ready to swear right here, right now before the devil?"
-    },
-
-    {
-        song:"Baby Got Back",
-        artist: "Sir Mix-a-Lot",
-        lyric: "I like big butts and I cannot lie. You other brothers can't deny",
-    },
-
-     {
-        song:"Du Hast",
-        artist: "Rammstein",
-        lyric:"Willst du, bis der Tod euch scheidet, treu ihr sein fur alle tage?",
-    },
-
-     {
-        song:"Take on me",
-        artist: "A-ha",
-        lyric: "Take on me, take me on, I'll be gone in a day or two",
-    },
-
-
-
-];
-
 var movieQuotes = [
     {
         movie: "There Will Be Blood",
